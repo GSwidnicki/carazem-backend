5ef7d68bd16b5839dfc794c2f5d8091f57011392
Pawel Koscielny <pawel.koscielny@outlook.com>
2016-07-20 21:08:42 +0200

    Adding working HIbernate

--- src/main/java/com.carazem/config/HibernateConfig.java
+++ src/main/java/com.carazem/config/HibernateConfig.java
@@ -4,9 +4,11 @@ import com.carazem.config.ConfigService;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
@@ -15,26 +17,19 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.carazem")
+@EnableTransactionManagement
+@ComponentScan("com.carazem")
 public class HibernateConfig {
 
     @Autowired
     ConfigService configService;
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(new String[] { "com.carazem" });
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
 
     @Bean
     public DataSource dataSource() {
@@ -48,36 +43,49 @@ public class HibernateConfig {
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory =
+                new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactory.setDataSource(dataSource);
+
+        // Classpath scanning of @Component, @Service, etc annotated class
+        entityManagerFactory.setPackagesToScan("com.carazem");
+
+        // Vendor adapter
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
 
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan("com.carazem");
-        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
-        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
-        entityManagerFactoryBean.afterPropertiesSet();
+        // Hibernate properties
+        Properties additionalProperties = new Properties();
+        additionalProperties.put("hibernate.show_sql", configService.get(Keys.HIBERNATE_SHOW_SQL));
+        additionalProperties.put("hibernate.show_sql", configService.get(Keys.HIBERNATE_FORMAT_SQL));
+        additionalProperties.put("hibernate.show_sql", configService.get(Keys.HIBERNATE_GENERATE_STATISTICS));
+        additionalProperties.put("hibernate.hbm2ddl.auto", configService.get(Keys.HIBERNATE_HBM2DDL));
+        entityManagerFactory.setJpaProperties(additionalProperties);
 
-        return entityManagerFactoryBean.getObject();
-
-    }
-
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.hbm2ddl.auto", configService.get(Keys.HIBERNATE_HBM2DDL));
-        properties.put("hibernate.show_sql", configService.get(Keys.HIBERNATE_SHOW_SQL));
-        properties.put("hibernate.format_sql", configService.get(Keys.HIBERNATE_FORMAT_SQL));
-        properties.put("hibernate.generate_statistics", configService.get(Keys.HIBERNATE_GENERATE_STATISTICS));
-        return properties;
+        return entityManagerFactory;
     }
 
     @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory s) {
-        HibernateTransactionManager txManager = new HibernateTransactionManager();
-        txManager.setSessionFactory(s);
-        return txManager;
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager =
+                new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(
+                entityManagerFactory.getObject());
+        return transactionManager;
     }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private LocalContainerEntityManagerFactoryBean entityManagerFactory;
 
 
 }
--- src/main/java/com.carazem/controller/IndexController.java
+++ src/main/java/com.carazem/controller/IndexController.java
@@ -4,8 +4,12 @@ import com.carazem.config.ConfigService;
 import com.carazem.config.Keys;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created by RENT on 2016-07-18.
@@ -15,18 +19,17 @@ public class IndexController {
     @Autowired
     ConfigService configService;
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @RequestMapping("/")
     public String home() {
         System.out.println(configService.get(Keys.SERVER_PORT));
 
-//        String query = "SELECT schemaname FROM pg_catalog.pg_tables LIMIT 1";
+        //Test polaczenia
+        String query = "SELECT schemaname FROM pg_catalog.pg_tables LIMIT 1";
+        System.out.println(entityManager.createNativeQuery(query).getSingleResult().toString());
 
-        System.out.print(sessionFactory.getCurrentSession().isConnected());
-
-        System.out.println("DUPA");
         return "Dziala backend!";
     }
 
