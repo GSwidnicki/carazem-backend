d6ba60a20de723eea4069a4a0dad3540e04fcba5
Jaroslaw_B <bucior.jaroslaw@gmail.com>
2016-07-27 19:21:00 +0200

    fixed validation from json

--- src/main/java/com.carazem/ride/RideController.java
+++ src/main/java/com.carazem/ride/RideController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class RideController {
     }
 
     @RequestMapping(method = POST)
-    public void addRide(@Validated Ride ride) {
+    public void addRide(@RequestBody @Validated Ride ride) {
          rideService.addRide(ride);
     }
 
