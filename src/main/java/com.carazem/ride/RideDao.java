4f6ad47adb4ed2701fd3387fe691f38b1a3738dd
Jaroslaw_B <bucior.jaroslaw@gmail.com>
2016-07-27 20:31:58 +0200

    working pagination

--- src/main/java/com.carazem/ride/RideDao.java
+++ src/main/java/com.carazem/ride/RideDao.java
@@ -2,6 +2,8 @@ package com.carazem.ride;
 
 import com.carazem.ride.Ride;
 import com.carazem.user.User;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import java.util.List;
 @Repository
 public interface RideDao extends JpaRepository<Ride, Long> {
 
-    List<Ride> findByCityFromAndCityToAndRideDateGreaterThan(String cityFrom, String cityTo, Timestamp rideDate);
+    List<Ride> findByCityFromAndCityToAndRideDateGreaterThan(String cityFrom, String cityTo, Timestamp rideDate, Pageable pageable);
 
 
     Ride findByDriverIdAndRideDate(Long id, Timestamp rideDate);
